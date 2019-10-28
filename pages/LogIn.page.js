8241898dd7a2ad9f78c5f52f5a0dacc4ef77bba3
Petlas88 <42611458+Petlas88@users.noreply.github.com>
2019-10-28 21:43:42 +0100

    Change registration modal, update styling

--- pages/LogIn.page.js
+++ pages/LogIn.page.js
@@ -2,22 +2,22 @@ const LogInPage = {
   template: `
     <div class="container">
         <!--REGISTRATION MODAL-->
-        <transition name="bounce">
+        <transition name="slide-up">
         <div v-if="visibleMod" id="reg-modal">
             <div id="reg-modal-header">
                 <div @click="visibleMod=!visibleMod" id="reg-modal-collapse-btn">&#10006</div>
             </div>
             <div id="reg-modal-body">
                 <h1 id="mod-title">{{modTitle}}</h1>
-                <h2>Fullt navn</h2>
+                <p id=full-name>Fullt navn</p>
                 <input type="text" name="" class="reg-input" placeholder="Fullt navn" id="reg-name"/>
-                <h2>Fødselsdato</h2>
+                <p>Fødselsdato</p>
                 <input type="date" class="reg-input" placeholder="Fødselsdato" id="reg-dob"/>
-                <h2>E-post</h2>
+                <p>E-post</p>
                 <input type="email" class="reg-input" placeholder="E-post" id="reg-mail"/>
-                <h2>Passord</h2>
+                <p>Passord</p>
                 <input type="password" class="reg-input" placeholder="Passord" id="reg-pass"/>
-                <h2>Gjenta passord</h2>
+                <p>Gjenta passord</p>
                 <input type="password" class="reg-input" placeholder="Gjenta passord" id="reg-rep-pass"/>
                 <button id="reg-btn">Registrer deg</button>
             </div>
@@ -33,7 +33,7 @@ const LogInPage = {
             <div class="col-12">
                 <h2><b>{{userTxt}}</b></h2>
                 <input type="text" class="login-input" id="username-input" placeholder="Brukernavn(e-post)"/>
-                <h2><b>{{passTxt}}</b></h2></p>
+                <h2><b>{{passTxt}}</b></h2>
                 <input type="password" class="login-input" id="password-input" placeholder="Passord"/>
                 <br/>
                 <button id="login-btn" @click="checkInput">Logg inn</button>
